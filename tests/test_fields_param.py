43973aae6142d3516b7d365282ecbeb5de77aab6
EduardoPinzon <n6eduardo@gmail.com>
2025-12-11 15:26:50 -0600

    Fix: add WebDriverWait and centralized selectors for practice form fields

--- tests/test_fields_param.py
+++ tests/test_fields_param.py
@@ -1,20 +1,40 @@
-# tests/test_fields_param.py
 import pytest
 from selenium.webdriver.common.by import By
-from utils.data_factory import user_data
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-@pytest.mark.parametrize("field_id,key", [
-    ("firstName", "first_name"),
-    ("lastName", "last_name"),
-    ("userEmail", "email"),
-    ("userNumber", "phone"),
+# Diccionario centralizado de selectores
+selectors = {
+    "first_name": (By.ID, "firstName"),
+    "last_name": (By.ID, "lastName"),
+    "email": (By.ID, "userEmail"),
+    "phone": (By.ID, "userNumber"),
+}
+
+# Datos de prueba
+def user_data():
+    return {
+        "first_name": "Eduardo",
+        "last_name": "Reyna",
+        "email": "eduardo.reyna@example.com",
+        "phone": "8112345678"
+    }
+
+@pytest.mark.parametrize( "key", [
+    "first_name",
+    "last_name",
+    "email",
+    "phone",
 ])
-def test_field_filling(driver, field_id, key):
-    driver.get("https://demoqa.com/automation-practice_form")
+def test_field_filling(driver, key):
+    driver.get("https://demoqa.com/automation-practice-form")
     data = user_data()
 
-    field = driver.find_element(By.ID, field_id)
+    by, value = selectors[key]
+
+    field = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((by, value))
+    )
     field.send_keys(data[key])
 
-    # Validación de que el input contiene lo que enviamos
     assert field.get_attribute("value") == data[key]
